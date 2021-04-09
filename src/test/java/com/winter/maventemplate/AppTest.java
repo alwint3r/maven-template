aeef77de50b7b94918af028587b764e3343ba9b8
Alwin Arrasyid <alwin.ridd@gmail.com>
2021-04-09 19:58:52 +0700

    Initial commit

---
+++ src/test/java/com/winter/maventemplate/AppTest.java
@@ -0,0 +1,16 @@
+package com.winter.maventemplate;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void dummyTest() {
+        assertEquals(1, 1);
+    }
+}
